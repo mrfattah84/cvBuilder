c90d3624182358845903dbe3b06c9e9ea99e99d7
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2024-10-11 01:03:27 +0330

    log in added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { useState } from 'react';
+import { useUser } from '@clerk/clerk-react';
 import './styles/App.css';
+import { Outlet, Navigate } from 'react-router-dom';
+import Header from './components/ui/custom/Header';
 
 function App() {
-  return <h1>Hiii!!</h1>;
+  const { user, isLoaded, isSignedIn } = useUser();
+
+  if (!isSignedIn && isLoaded) {
+    return <Navigate to={'/auth/signIn'} />;
+  }
+
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import SignInPage from './auth/signIn/index.tsx';
+import Home from './home/index.tsx';
+import Dashboard from './dashboard/index.tsx';
+import { ClerkProvider } from '@clerk/clerk-react';
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: '/dashboard',
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/auth/signIn',
+    element: <SignInPage />,
+  },
+]);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+);
