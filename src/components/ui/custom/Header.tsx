16d63c2ea9c47320c3bb6b59b1334ca72d490cfe
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2024-10-14 14:36:20 +0330

    added header

--- src/components/ui/custom/Header.tsx
+++ src/components/ui/custom/Header.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Button } from '../button';
+import { Link } from 'react-router-dom';
+import { UserButton, useUser } from '@clerk/clerk-react';
 
 function Header() {
+  const { user, isSignedIn } = useUser();
   return (
-    <div>
-      <img src="/logo.svg" width={100} height={100} />
+    <div className="p-3 px-5 flex justify-between shadow-md">
+      <Link to={'/'}>
+        <img src="/logo.svg" width={100} height={100} />
+      </Link>
+
+      {isSignedIn ? (
+        <div className="flex gap-2 items-center">
+          <Link to={'/dashboard'}>
+            <Button variant={'outline'}>Dashboard</Button>
+          </Link>
+          <UserButton />
+        </div>
+      ) : (
+        <Link to={'/auth/signIn'}>
+          <Button>Get Started</Button>
+        </Link>
+      )}
     </div>
   );
 }
