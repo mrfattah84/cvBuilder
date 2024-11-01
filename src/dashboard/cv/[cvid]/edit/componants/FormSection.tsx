e7aa659f73a1841053ad1ae84b253feee9c9f6b9
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2024-11-02 02:24:58 +0330

    added theme color picker functionality

--- src/dashboard/cv/[cvid]/edit/componants/FormSection.tsx
+++ src/dashboard/cv/[cvid]/edit/componants/FormSection.tsx
@@ -1,23 +1,56 @@
 import { Button } from '@/components/ui/button';
 import { ArrowLeft, ArrowRight, Palette } from 'lucide-react';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import PersonalDetails from './forms/PersonalDetails';
 import Summery from './forms/Summery';
 import Experience from './forms/Experience';
 import Education from './forms/Education';
 import Skill from './forms/Skill';
 import { Navigate, useParams } from 'react-router-dom';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { HexColorPicker } from 'react-colorful';
+import { CVInfoContext } from '@/context/CVInfoContext';
+import GlobalApi from './../../../../../../service/GlobalApi';
+import { toast } from 'sonner';
 
 function FormSection() {
   const [activeFormIndex, setActiveFormIndex] = useState(0);
   const [enableNext, setEnableNext] = useState(false);
   const params = useParams();
+  const [color, setColor] = useState('#000');
+  const { CVInfo, setCVInfo } = useContext(CVInfoContext);
+
+  const handleColorChange = (color) => {
+    setColor(color);
+    setCVInfo({ ...CVInfo, themeColor: color });
+  };
+
   return (
     <div>
       <div className="flex justify-between">
-        <Button variant={'outline'}>
-          <Palette /> Theme
-        </Button>
+        <Popover
+          onOpenChange={() => {
+            const data = {
+              data: { themeColor: color },
+            };
+            GlobalApi.UpdateCVDetail(params.cvid, data).then(() => {
+              toast('Theme color updated successfully');
+            });
+          }}
+        >
+          <PopoverTrigger>
+            <div className="flex gap-2">
+              <Palette /> Theme
+            </div>
+          </PopoverTrigger>
+          <PopoverContent>
+            <HexColorPicker color={color} onChange={handleColorChange} />
+          </PopoverContent>
+        </Popover>
         <div className="flex gap-2">
           {activeFormIndex > 0 && (
             <Button onClick={() => setActiveFormIndex(activeFormIndex - 1)}>
