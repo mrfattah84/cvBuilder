7c04d8122f3f1d36944f126c35029e37b8efc895
mrfattah84 <148151611+mrfattah84@users.noreply.github.com>
2024-11-02 00:08:25 +0330

    added share functionality

--- src/myCV/[CVID]/view/index.tsx
+++ src/myCV/[CVID]/view/index.tsx
@@ -5,6 +5,8 @@ import GlobalApi from './../../../../service/GlobalApi';
 import { useParams } from 'react-router-dom';
 import CVPreview from '@/dashboard/cv/[cvid]/edit/componants/CVPreview';
 import { Button } from '@/components/ui/button';
+import { RWebShare } from 'react-web-share';
+import { Download, Share, Share2Icon } from 'lucide-react';
 
 function ViewCV() {
   const [CVInfo, setCVInfo] = useState();
@@ -23,17 +25,29 @@ function ViewCV() {
 
   const HandleDownload = () => {
     window.print();
-  }
+  };
 
   return (
-    <div id='cv-view'>
+    <div id="cv-view">
       <CVInfoContext.Provider value={{ CVInfo, setCVInfo }}>
         <Header />
         <div className="flex flex-col items-center justify-center">
           <h1>Here is your CV!</h1>
           <div className="flex w-full px-6 py-2 justify-between">
-            <Button onClick={HandleDownload}>Download</Button>
-            <Button>Share</Button>
+            <Button onClick={HandleDownload}>
+              Download <Download className="pl-2" />
+            </Button>
+            <RWebShare
+              data={{
+                text: 'Hello, check out my CV',
+                url: `${window.location.href}`,
+                title: `${CVInfo?.firstName}'s CV`,
+              }}
+            >
+              <Button>
+                Share <Share2Icon className="pl-2" />
+              </Button>
+            </RWebShare>
           </div>
         </div>
         <CVPreview />
